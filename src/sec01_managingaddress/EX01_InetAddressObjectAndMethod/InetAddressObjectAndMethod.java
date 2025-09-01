414eaffa4755c137beaaa6be9a39a20fe0f39a09
kimdh-hyw <71587564+kimdh-hyw@users.noreply.github.com>
2025-09-01 10:38:48 +0900

    Update InetAddressObjectAndMethod.java
    .

--- src/sec01_managingaddress/EX01_InetAddressObjectAndMethod/InetAddressObjectAndMethod.java
+++ src/sec01_managingaddress/EX01_InetAddressObjectAndMethod/InetAddressObjectAndMethod.java
@@ -13,8 +13,8 @@ public class InetAddressObjectAndMethod {
 		//#1. InetAddress 객체 생성
 		//@1-1. 원격지IP 객체 생성
 		InetAddress ia1 = InetAddress.getByName("www.google.com");
-		InetAddress ia2 = InetAddress.getByAddress(new byte[] {(byte)172,(byte)217,(byte)161,36});
-		InetAddress ia3 = InetAddress.getByAddress("www.google.com", new byte[] {(byte)172,(byte)217,(byte)161,36});
+		InetAddress ia2 = InetAddress.getByAddress(new byte[] {(byte)142,(byte)250,(byte)206,(byte)196});
+		InetAddress ia3 = InetAddress.getByAddress("www.google.com", new byte[] {(byte)142,(byte)250,(byte)206,(byte)196});
 		System.out.println(ia1);
 		System.out.println(ia2);
 		System.out.println(ia3);
@@ -29,26 +29,24 @@ public class InetAddressObjectAndMethod {
 		
 		
 		//@1-3. 하나의 호스트가 여러 개의 IP를 가지고 있는 경우
-		InetAddress[] ia6 = InetAddress.getAllByName("www.naver.com");
-		System.out.println(Arrays.toString(ia6));
+		InetAddress[] ia6 = InetAddress.getAllByName("www.yahoo.com"); //20250901 기준 2개 리턴
+		System.out.println("서버수="+ia6.length +"  "+ Arrays.toString(ia6));
 		System.out.println();
 		
 		//#2. InetAddress 메서드
 		byte[] address = ia1.getAddress();
-		System.out.println(Arrays.toString(address));
-		System.out.println(ia1.getHostAddress());
+		System.out.println(Arrays.toString(address)); //byte[]의 값으로 반환(-128~127 사이의 값)
+		System.out.println(ia1.getHostAddress()); //0~255사이 값으로 출력
 		System.out.println(ia1.getHostName());
 		System.out.println();
 		
 		System.out.println(ia1.isReachable(1000)); //true
-		System.out.println(ia1.isLoopbackAddress()); //false
+		System.out.println(ia1.isLoopbackAddress()); //false 127.0.0.1 
 		System.out.println(ia1.isMulticastAddress()); //false 224-239.0-255.0-255.0-255
 		System.out.println(InetAddress.getByAddress(new byte[] {127,0,0,1}).isLoopbackAddress()); //true
 		System.out.println(InetAddress.getByAddress(new byte[] {(byte)234,(byte)234,(byte)234,(byte)234}).isMulticastAddress()); //true
 
-		
-		
-		
 	}
 }
+
 
