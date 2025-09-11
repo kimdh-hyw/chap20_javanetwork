d3639d8348eaaa3b19412ccf39014236e377333d
kimdh-hyw <71587564+kimdh-hyw@users.noreply.github.com>
2025-09-11 17:40:34 +0900

    Update TCP_Text_ServerSide.java
    .

--- src/sec02_tcpcommunication/EX03_TCP_Text/TCP_Text_ServerSide.java
+++ src/sec02_tcpcommunication/EX03_TCP_Text/TCP_Text_ServerSide.java
@@ -1,6 +1,6 @@
 package sec02_tcpcommunication.EX03_TCP_Text;
 
-/* TCP 기반 Text 전송을 위한 ServerSide */
+/* TCP 기반 Text 전송을 위한 ServerSide ("안녕하세요" 수신 후 "어서오세요" 회신)*/
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
@@ -44,4 +44,5 @@ public class TCP_Text_ServerSide {
 		
 	}
 }
+
 
