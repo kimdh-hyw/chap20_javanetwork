d2ca2c6f5bbd3e219d9c8e699e55f6e6311f00a3
kimdh-hyw <71587564+kimdh-hyw@users.noreply.github.com>
2025-09-18 18:24:41 +0900

    Update NonConnectedUDP_Text_ClientB.java
    .

--- src/sec03_udpcommunication/EX03_NonConnectedUDP_Text/NonConnectedUDP_Text_ClientB.java
+++ src/sec03_udpcommunication/EX03_NonConnectedUDP_Text/NonConnectedUDP_Text_ClientB.java
@@ -1,6 +1,6 @@
 package sec03_udpcommunication.EX03_NonConnectedUDP_Text;
 
-/* 비연결성(nonconnected) UDP를 이용한 Text 전송 (ClientA): 20000번 포트 바인딩 */
+/* 비연결성(nonconnected) UDP를 이용한 Text 전송 (ClientB): 20000번 포트 바인딩 (2025.09 수정) */
 
 import java.io.IOException;
 import java.net.DatagramPacket;
@@ -19,24 +19,25 @@ public class NonConnectedUDP_Text_ClientB {
 			ds = new DatagramSocket(20000);
 		} catch (SocketException e) { e.printStackTrace();}
 		
-		//#2. 데이터그램패킷 수신
+		//#2. 전송데이터 생성 + DatagramPacket 생성
+		byte[] sendData = "안녕하세요".getBytes();
+		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, new InetSocketAddress("localhost", 10000));
+		
+		//#3. 데이터그램패킷 전송
+		System.out.println("송신 데이터 : "+ new String(sendPacket.getData(), 0, sendPacket.getLength()));
+		try {
+			ds.send(sendPacket);
+		} catch (IOException e) {e.printStackTrace();}
+		
+		//#4. 데이터그램패킷 수신
 		byte[] receivedData = new byte[65508];
 		DatagramPacket receivedPacket = new DatagramPacket(receivedData, receivedData.length);
 		try {
 			ds.receive(receivedPacket);
 		} catch (IOException e) { e.printStackTrace();}
 		System.out.println("수신 데이터 : "+ new String(receivedPacket.getData(), 0, receivedPacket.getLength()));
-				
-		//#3. 전송데이터 생성 + DatagramPacket 생성
-		byte[] sendData = "반갑습니다.".getBytes();
-		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, receivedPacket.getSocketAddress());
-		
-		//#4. 데이터그램패킷 전송
-		System.out.println("송신 데이터 : "+ new String(sendPacket.getData(), 0, sendPacket.getLength()));
-		try {
-			ds.send(sendPacket);
-		} catch (IOException e) {e.printStackTrace();}
 		
 	}
 }
+
 
